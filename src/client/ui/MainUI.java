e8ba4ee6e022558a8861ec5a6c47117da74f6763
datetory <492121382@qq.com>
2014-07-21 17:14:21 +0800

    以前的

--- src/client/ui/MainUI.java
+++ src/client/ui/MainUI.java
@@ -21,22 +21,24 @@ public class MainUI extends JFrame{
 		this.setLocationRelativeTo(null);// 居中显示
 		this.setUndecorated(false);// 禁用此窗体装饰
 		this.setIconImage(new ImageIcon("images/logo.png").getImage());// 设置窗体的图标
-		this.setBackground(Color.white);// 设置背景为白色
+//		this.setBackground(Color.white);// 设置背景为白色
+		this.getContentPane().setBackground(Color.white);
 		
-		// 北边面板
-		JPanel northPane = this.createnorthPanel();
-		this.add(northPane, BorderLayout.NORTH);
-		// 中间面板
-		JPanel centerPane = createcenterPanel();
-		this.add(centerPane, BorderLayout.CENTER);
-		// 南边面板
-		JPanel southPanel = createsouthPanel();
-		this.add(southPanel,BorderLayout.SOUTH);
+//		// 北边面板
+//		JPanel northPane = this.createnorthPanel();
+//		this.add(northPane, BorderLayout.NORTH);
+//		// 中间面板
+//		JPanel centerPane = createcenterPanel();
+//		this.add(centerPane, BorderLayout.CENTER);
+//		// 南边面板
+//		JPanel southPanel = createsouthPanel();
+//		this.add(southPanel,BorderLayout.SOUTH);
 		
 		this.setVisible(true);
 		
 	}
 
+	
 	private JPanel createsouthPanel() {
 		return null;
 	}
