374d75f2245b02ae6ae3b5dbe122b9aa369c9980
Tangziyi <1174779123@qq.com>
2014-07-21 14:01:01 +0800

    ...

--- src/server/common/packages/SignRequestPackage.java
+++ src/server/common/packages/SignRequestPackage.java
@@ -5,8 +5,7 @@ public class SignRequestPackage extends ServerPackage{
 	private byte state;
 
 
-	public SignRequestPackage(byte state) {
-		super();
+	
 
 	public SignRequestPackage( byte state) {
 		this.state = state;
