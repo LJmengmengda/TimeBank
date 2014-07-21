2c0191b896d4e08c97ee2f9e5b52b7676a465f5e
王丹 <492121382@qq.com>
2014-07-21 10:24:16 +0800

    Update SignupUI.java
    更新了注册界面

--- src/client/ui/SignupUI.java
+++ src/client/ui/SignupUI.java
@@ -1,204 +1,196 @@
 package client.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.Font;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-import client.backup.signup.SignupListener;
 /**
  * 注册界面
- * @author 徐意
- *
+ * 
+ * @author 王丹
+ * 
  */
+public class SignupUI extends JFrame {
 
-/**
- * @author Administrator
- *
- */
-public class SignupUI extends JFrame{
-	
-	public static void main(String[] args) {
-		new SignupUI();
+	// 测试
+	public static void main(String[] arg) {
+		SignupUI SU = new SignupUI();
+		SU.init();
 	}
-	//属性
-	private JTextField userNameField;//用户名输入框
-	private JPasswordField passWordField1;//密码输入框
-	private JPasswordField passWordField2;//密码输入检测框
-	
-	private JButton signup;//注册
-	private JButton login;//登陆
-	
-	private SignupListener s;
-	
-	//界面初始化
-	public SignupUI(){
-		this.setSize(350,600);
+
+	// 属性
+	private JTextField UserNameField;// 姓名输入框
+	private JPasswordField PassWordField1;// 密码输入框
+	private JPasswordField PassWordField2;// 密码输入检测框
+	private JButton SignUp;// 注册按钮
+
+	/**
+	 * 注册界面初始化
+	 */
+	public void init() {
+		this.setSize(350, 600);
 		this.setTitle("时间银行");
-		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-		this.setUndecorated(false);
-		this.setLayout(new BorderLayout());
-//		this.setBackground();
-		
-		
-		s = new SignupListener(this);
-		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 设置关闭
+		this.setLocationRelativeTo(null);// 居中显示
+		this.setUndecorated(false);// 禁用此窗体装饰
+		this.setIconImage(new ImageIcon("images/logo.png").getImage());// 设置窗体的图标
+		this.setBackground(Color.white);// 设置背景为白色
+//		this.setResizable(false);// 设置不可改变大小
+
+		// 北
+		JPanel northPane = this.createnorthPanel();
+		this.add(northPane, BorderLayout.NORTH);
+		// 中间
 		JPanel centerPane = createcenterPanel();
-		this.add(centerPane,BorderLayout.CENTER);
-		JPanel northPane = createnorthPanel();
-		this.add(northPane,BorderLayout.NORTH);
-		JPanel westPane = createwestPanel();
-		this.add(westPane,BorderLayout.WEST);
-		JPanel eastPane = createeastPanel();
-		this.add(eastPane,BorderLayout.EAST);
-		JPanel southPanel = createsouthPanel();
-		this.add(southPanel,BorderLayout.SOUTH);
-		
-		
-		
-		
-		
+		this.add(centerPane, BorderLayout.CENTER);
+
+		// JPanel westPane = createwestPanel();
+		// this.add(westPane,BorderLayout.WEST);
+		// JPanel eastPane = createeastPanel();
+		// this.add(eastPane,BorderLayout.EAST);
+		// JPanel southPanel = createsouthPanel();
+		// this.add(southPanel,BorderLayout.SOUTH);
+
 		this.setVisible(true);
 	}
-	
-	
-	public JTextField getUserNameField() {
-		return userNameField;
-	}
 
-	public void setUserNameField(JTextField userNameField) {
-		this.userNameField = userNameField;
-	}
-
-	
-	public JPasswordField getPassWordField1() {
-		return passWordField1;
-	}
-
-
-	public void setPassWordField1(JPasswordField passWordField1) {
-		this.passWordField1 = passWordField1;
-	}
-
-
-	public JPasswordField getPassWordField2() {
-		return passWordField2;
-	}
-
-
-	public void setPassWordField2(JPasswordField passWordField2) {
-		this.passWordField2 = passWordField2;
-	}
-
-
-	public JButton getSignUp() {
-		return signup;
-	}
-	/**set方法
-	 * @param signUp 注册按钮
+	/**
+	 * 北边部分
 	 */
-	public void setSignUp(JButton signUp) {
-		this.signup = signUp;
-	}
-
-	public JButton getLogin() {
-		return login;
-	}
-
-	public void setLogin(JButton login) {
-		this.login = login;
-	}
-	
-	
-	public JPanel createcenterPanel(){
-		JPanel centerPanel = new JPanel();
-		centerPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-	
-		JLabel UserName = new JLabel("用户名");
-		
-		
-		userNameField = new JTextField();
-		userNameField.setEditable(true);
-		userNameField.setPreferredSize(new Dimension(190,25));
-		//TODO tianhjia
-		
-		
-		
-		
-		JLabel PassWord1 = new JLabel("密码");
-		
-		passWordField1 =new JPasswordField(17);
-		
-		JLabel PassWord2 = new JLabel("密码确认");
-		passWordField2 =new JPasswordField(17);
-		
-		centerPanel.add(UserName);
-		centerPanel.add(userNameField);
-	
-		centerPanel.add(PassWord1);
-		centerPanel.add(passWordField1);
-		centerPanel.add(PassWord2);
-		centerPanel.add(passWordField2);
-		centerPanel.setOpaque(false);
-		
-		return centerPanel;
-	}
-	
-	
-	public JPanel createnorthPanel(){
+	public JPanel createnorthPanel() {
 		JPanel northPanel = new JPanel();
-		northPanel.setPreferredSize(new Dimension(0,200));
-		northPanel.setOpaque(false);
+		northPanel.setLayout(new BorderLayout());
+		northPanel.setPreferredSize(new Dimension(0, 250));
+		//注册logo设置
+		ImageIcon logo = new ImageIcon("images/logo2.png");//注册logo
+		JLabel jlogo = new JLabel(logo);
+		jlogo.setBounds(65, 65, 200, 200);
+		northPanel.add(jlogo,BorderLayout.CENTER);
+		
+		//TimeBanklogo设置
+		JLabel tlogo = new JLabel("Time Bank");
+		tlogo.setFont(new Font("微软雅黑", Font.BOLD, 28));// 设置标签字体
+		tlogo.setForeground(Color.black);// 设置字体颜色
+		tlogo.setHorizontalAlignment(JLabel.CENTER);
+		tlogo.setVerticalAlignment(JLabel.CENTER);
+		tlogo.setBounds(50, 0, 100, 30);
+		northPanel.add(tlogo,BorderLayout.SOUTH);
+		
 		return northPanel;
 	}
-	
-	
-	
-	public JPanel createwestPanel(){
+
+	public JPanel createcenterPanel() {
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new BorderLayout());
+
+		JLabel UserName = new JLabel("用户名");
+
+		UserNameField = new JTextField();
+		UserNameField.setEditable(true);
+		UserNameField.setPreferredSize(new Dimension(190, 25));
+
+		JLabel PassWord1 = new JLabel("密码");
+
+		PassWordField1 = new JPasswordField(17);
+
+		JLabel PassWord2 = new JLabel("密码确认");
+		PassWordField2 = new JPasswordField(17);
+
+		centerPanel.add(UserName);
+		centerPanel.add(UserNameField);
+
+		centerPanel.add(PassWord1);
+		centerPanel.add(PassWordField1);
+		centerPanel.add(PassWord2);
+		centerPanel.add(PassWordField2);
+		centerPanel.setOpaque(false);
+
+		return centerPanel;
+	}
+
+	public JPanel createwestPanel() {
 		JPanel westPanel = new JPanel();
 		westPanel.setPreferredSize(new Dimension(100, 0));
 		westPanel.setOpaque(false);
 		return westPanel;
 	}
-	
-	public JPanel createeastPanel(){
+
+	public JPanel createeastPanel() {
 		JPanel eastPanel = new JPanel();
-		eastPanel.setPreferredSize(new Dimension(100,0));
+		eastPanel.setPreferredSize(new Dimension(100, 0));
 		eastPanel.setOpaque(false);
 		return eastPanel;
 	}
-	
-	public JPanel createsouthPanel(){
+
+	public JPanel createsouthPanel() {
 		JPanel southPanel = new JPanel();
-		southPanel.setPreferredSize(new Dimension(100,47));
+		southPanel.setPreferredSize(new Dimension(100, 47));
 		southPanel.setOpaque(false);
-		
-		signup = new JButton("注册用户");
-		signup.addActionListener(s);
-	    southPanel.add(signup);
-	    
-	    
-	    return southPanel;
+
+		SignUp = new JButton("注册用户");
+
+		southPanel.add(SignUp);
+
+		return southPanel;
 	}
-	
-	
-	//设置背景图片的方法
-	private void setBackground(){
+
+	// 设置背景图片的方法
+	private void setBackground() {
 		ImageIcon image = new ImageIcon("images/background.jpg");
 		JLabel imageLabel = new JLabel(image);
 		imageLabel.setBounds(0, 0, this.getWidth(), this.getHeight());
-		
-		this.getLayeredPane().add(imageLabel,new Integer(Integer.MIN_VALUE));
-		
+
+		this.getLayeredPane().add(imageLabel, new Integer(Integer.MIN_VALUE));
+
 		JPanel content = (JPanel) this.getContentPane();
 		content.setOpaque(false);
+	}
+
+	public JTextField getUserNameField() {
+		return UserNameField;
+	}
+
+	public void setUserNameField(JTextField userNameField) {
+		UserNameField = userNameField;
+	}
+
+	public JPasswordField getPassWordField1() {
+		return PassWordField1;
+	}
+
+	public void setPassWordField1(JPasswordField passWordField1) {
+		PassWordField1 = passWordField1;
+	}
+
+	public JPasswordField getPassWordField2() {
+		return PassWordField2;
+	}
+
+	public void setPassWordField2(JPasswordField passWordField2) {
+		PassWordField2 = passWordField2;
+	}
+
+	public JButton getSignUp() {
+		return SignUp;
+	}
+
+	/**
+	 * set方法
+	 * 
+	 * @param signUp
+	 *            注册按钮
+	 */
+	public void setSignUp(JButton signUp) {
+		SignUp = signUp;
 	}
 }
