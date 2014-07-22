917aa9fea2207bb44f0ece6d57ae6530c07b3f77
xuyi <540767457@qq.com>
2014-07-22 17:01:36 +0800

    那你或多

---
+++ src/client/backup/main/jbRequestListener.java
@@ -0,0 +1,18 @@
+package client.backup.main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ * 租赁按钮监听器
+ * @author 徐意
+ *
+ */
+public class jbRequestListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println("租赁");
+		
+	}
+
+}
